6d9009b77c4a890821bdf7d149942958c5830778
Adrien Baffioni <baffioniadrien23@gmail.com>
2025-07-16 11:50:09 +0200

    home style

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ContentData} from '../../shared/models/content-data';
 import {ContentText} from '../../shared/models/content-text';
 import {PageEnum} from '../../shared/models/page-enum';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,17 +11,41 @@ import {PageEnum} from '../../shared/models/page-enum';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Input() data!: ContentData;
   @Input() text!: ContentText;
   @Input() page!: PageEnum;
   @Input() screenWidth!: number
 
-  selectedItem = 0;
+  selectedItem = 'home';
   visible = false;
 
-  selectItem(index: number) {
-    this.selectedItem = index;
+  private routerSub!: Subscription;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.updateSelectedItem(this.router.url); // initialise la sélection
+
+    this.routerSub = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateSelectedItem(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerSub?.unsubscribe();
+  }
+
+  updateSelectedItem(url: string) {
+    const cleanUrl = url.replace(/^\/+/, '').split('/')[0];
+    this.selectedItem = cleanUrl || 'home';
+  }
+
+  selectItem(item: string) {
+    this.selectedItem = item;
   }
 
   openMenu() {
