c84222b6a37a132bdf2495db07f2161c4bfd0e84
Adrien Baffioni <baffioniadrien23@gmail.com>
2025-07-24 12:24:15 +0200

    get data from files if api doesn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import {ContentService} from './shared/services/content.service';
 import {PageEnum} from './shared/models/page-enum';
 import {PageData} from './shared/models/page-data';
+import { unflattenText } from './shared/utils/helper';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import {PageData} from './shared/models/page-data';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-
+  title = "Space tourism";
   texts!: {[p: string]: any}
   page!: PageEnum;
   screenWidth!: number;
@@ -22,12 +24,74 @@ export class AppComponent implements OnInit {
   ) {
   }
 
-  async ngOnInit() {
-    await this.contentService.loadTexts();
-    this.getScreenSize();
-    this.texts = this.contentService.getAllTexts();
-    this.data = this.contentService.getData();
+  ngOnInit() {
+    this.getTexts();
+
+    this.getData();
     this.page = PageEnum.HOME;
+
+    this.getScreenSize();
+  }
+
+  getTexts()  {
+    this.contentService.getAllTextsFromDB().subscribe({
+      next: data => {
+        this.texts = unflattenText(data);
+        this.afterTextsLoaded();
+      },
+      error: () => {
+        console.warn("Backend inaccessible, chargement des textes depuis le fichier");
+        this.contentService.getAllTextsFromFile().subscribe({
+          next: fileData => {
+            this.texts = fileData;
+            this.afterTextsLoaded();
+          },
+          error: () => {
+            console.error("Impossible de charger les textes depuis le fichier.");
+          }
+        });
+      }
+    });
+  }
+
+  getData() {
+    forkJoin({
+      dest: this.contentService.getDestinations(),
+      crew: this.contentService.getCrews(),
+      tech: this.contentService.getTechnologies()
+    }).subscribe({
+      next: result => {
+      if (result.dest.length && result.crew.length && result.tech.length) {
+        this.data = result;
+      } else {
+        console.warn("Chargement partiel ou vide, lecture du fichier de secours");
+        this.loadDataFromFile();
+      }
+    },
+      error: () => {
+      console.warn("Échec du chargement des données, lecture du fichier de secours");
+      this.loadDataFromFile();
+    }
+    });
+  }
+
+  loadDataFromFile() {
+    this.contentService.getDataFromFile().subscribe({
+      next: fileData => {
+        this.data = {
+          dest: fileData.destinations,
+          crew: fileData.crew,
+          tech: fileData.technology
+        };
+        console.log("Données depuis fichier :", this.data);
+      },
+      error: () => {
+        console.error("Échec du chargement des données de secours");
+      }
+    });
+  }
+
+  afterTextsLoaded() {
     this.navItems = this.texts['nav'] || [];
   }
 
--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -1,67 +1,41 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {BehaviorSubject, firstValueFrom, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Destination} from '../models/destination';
 import {Crew} from '../models/crew';
 import {Technology} from '../models/technology';
-import {PageData} from '../models/page-data';
 
-const url = "https://space-tourism-backend-p27f.onrender.com";
+const dbUrl = "https://space-tourism-backend-p27f.onrender.com";
+const fileUrl = 'assets/data/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-  private textsSubject = new BehaviorSubject<{ [key: string]: any }>({});
 
   constructor(private http: HttpClient) {}
 
-  async loadTexts(): Promise<void> {
-    try {
-      const data = await firstValueFrom(this.http.get<{ [key: string]: any }>(url + '/texts'));
-      this.textsSubject.next(data || {});
-    } catch (error) {
-      console.error('Erreur lors du chargement des textes', error);
-    }
+  getAllTextsFromDB(): Observable<{ [key: string]: any }> {
+    return this.http.get<{ [key: string]: any }>(dbUrl + '/texts');
   }
 
-  getAllTexts(): { [key: string]: any } {
-    return this.textsSubject.value;
+  getAllTextsFromFile() {
+    return this.http.get<any>(fileUrl + 'text.json');
   }
 
   getDestinations(): Observable<Destination[]> {
-    return this.http.get<Destination[]>(url + '/data/destinations');
+    return this.http.get<Destination[]>(dbUrl + '/data/destinations');
   }
 
   getCrews(): Observable<Crew[]> {
-    return this.http.get<Crew[]>(url + '/data/crews');
+    return this.http.get<Crew[]>(dbUrl + '/data/crews');
   }
 
   getTechnologies(): Observable<Technology[]> {
-    return this.http.get<Technology[]>(url + '/data/technologies');
+    return this.http.get<Technology[]>(dbUrl + '/data/technologies');
   }
 
-  getData() {
-    let pageData: PageData = {
-      dest: [],
-      crew: [],
-      tech: []
-    };
-    this.getDestinations().subscribe(
-      data => {
-        pageData.dest = data;
-      }
-    );
-    this.getCrews().subscribe(
-      data => {
-        pageData.crew = data;
-      }
-    );
-    this.getTechnologies().subscribe(
-      data => {
-        pageData.tech = data;
-      }
-    );
-    return pageData;
+  getDataFromFile() {
+    return this.http.get<any>(fileUrl + 'data.json');
   }
 }
